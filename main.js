2ff859242f5e11f4fd56e365af3e0a741e44b62a
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-05-24 19:42:57 -0500

    Operador de Adicion

---
+++ main.js
@@ -0,0 +1,8 @@
+let nombre = "Oscar Yesid";
+
+// Significado
+// let nombreCompleto = nombre + " Cárdenas Santos";
+//Abreviado
+// nombre += " Castro Escamilla";
+
+document.body.insertAdjacentText("beforebegin", nombre);
