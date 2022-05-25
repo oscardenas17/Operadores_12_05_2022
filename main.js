56cad8a1131df8111c79379f7d361d0a1a879b96
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-05-24 19:54:59 -0500

    Operador Logico AND 'y'

--- main.js
+++ main.js
@@ -1,40 +1,52 @@
-let x = 10;
-let y = 3;
-let res = 0;
+let A = Number(prompt("Ingrese el valor de la variable A\n 0 = false\n 1 = true"));
+let B = Number(prompt("Ingrese el valor de la variable B\n 0 = false\n 1 = true"));
 
-let sel = document.querySelectorAll("em");
-sel[0].insertAdjacentText("beforeend", x);
-sel[1].insertAdjacentText("beforeend", y);
-sel[2].insertAdjacentText("beforeend", res);
-
-res = x + y;
-document.body.insertAdjacentHTML("beforeend", `
-    <div>
-        <b>Significado</b>
-    </div>
-    <hr>
-    <div>
-        <em>res = x + y;</em>
-        <br>
-        <mark>res</mark> : <em>${res}</em>
-    </div>
+document.querySelector("#operadorAND").insertAdjacentHTML("beforeend", `
+    <tr class="resultado">
+        <td>${A}</td>
+        <td>${B}</td>
+        <td>${A && B}</td>
+    </tr>
 `);
 
 
-// Abreviado
-x += y;
-document.body.insertAdjacentHTML("beforeend", `
-    <br>
-    <div>
-        <b>Abreviado</b>
-    </div>
-    <hr>
-    <div>
-        <em>x += y;</em>
-        <br>
-        <mark>x</mark> : <em>${x}</em>
-    </div>
-`);
+// let x = 10;
+// let y = 3;
+// let res = 0;
+
+// let sel = document.querySelectorAll("em");
+// sel[0].insertAdjacentText("beforeend", x);
+// sel[1].insertAdjacentText("beforeend", y);
+// sel[2].insertAdjacentText("beforeend", res);
+
+// res = x + y;
+// document.body.insertAdjacentHTML("beforeend", `
+//     <div>
+//         <b>Significado</b>
+//     </div>
+//     <hr>
+//     <div>
+//         <em>res = x + y;</em>
+//         <br>
+//         <mark>res</mark> : <em>${res}</em>
+//     </div>
+// `);
+
+
+// // Abreviado
+// x += y;
+// document.body.insertAdjacentHTML("beforeend", `
+//     <br>
+//     <div>
+//         <b>Abreviado</b>
+//     </div>
+//     <hr>
+//     <div>
+//         <em>x += y;</em>
+//         <br>
+//         <mark>x</mark> : <em>${x}</em>
+//     </div>
+// `);
 
 
 
