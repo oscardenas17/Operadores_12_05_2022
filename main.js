c34b514fe9460cbf900d362497c06fc24ddfc308
GRUPODIGITAL\oscar.cardenas <GRUPODIGITAL\oscar.cardenas>
2022-05-24 19:45:24 -0500

    Operador Arigmetico Suma

--- main.js
+++ main.js
@@ -1,8 +1,49 @@
-let nombre = "Oscar Yesid";
+let x = 10;
+let y = 3;
+let res = 0;
 
-// Significado
-// let nombreCompleto = nombre + " Cárdenas Santos";
-//Abreviado
-// nombre += " Castro Escamilla";
+let sel = document.querySelectorAll("em");
+sel[0].insertAdjacentText("beforeend", x);
+sel[1].insertAdjacentText("beforeend", y);
+sel[2].insertAdjacentText("beforeend", res);
 
-document.body.insertAdjacentText("beforebegin", nombre);
+res = x + y;
+document.body.insertAdjacentHTML("beforeend", `
+    <div>
+        <b>Significado</b>
+    </div>
+    <hr>
+    <div>
+        <em>res = x + y;</em>
+        <br>
+        <mark>res</mark> : <em>${res}</em>
+    </div>
+`);
+
+
+// Abreviado
+x += y;
+document.body.insertAdjacentHTML("beforeend", `
+    <br>
+    <div>
+        <b>Abreviado</b>
+    </div>
+    <hr>
+    <div>
+        <em>x += y;</em>
+        <br>
+        <mark>x</mark> : <em>${x}</em>
+    </div>
+`);
+
+
+
+
+// let nombre = "Oscar Yesid";
+
+// // Significado
+// // let nombreCompleto = nombre + " Cárdenas Santos";
+// //Abreviado
+// // nombre += " Castro Escamilla";
+
+// document.body.insertAdjacentText("beforebegin", nombre);
